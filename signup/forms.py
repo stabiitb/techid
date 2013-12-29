ba06f3b31c1b3e18f525b9bbe4270ec5a28a0384
prithvirajbilla <prithviraj.m.billa@gmail.com>
2013-12-30 03:43:42 +0530

    :+1: added forms

---
+++ signup/forms.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template.loader import get_template
+from django.template import Context
+from datetime import datetime
+import datetime as dt
+from django.http import HttpResponse,HttpResponseRedirect,Http404,HttpResponseNotFound
+
+import json
+from django.core.exceptions import ValidationError
+
+import sys
+
+from django import forms
+from django.forms import ModelForm
+from django_select2 import *
+
+from signup.models import *
+from misc.models import *
+
+class SignupForm(ModelForm):
+	class Meta:
+		model = User
+		exclude = ['is_active','is_admin','email','ldap_username','rollno']
+		fields = ['first_name','last_name','department','year','hostel','room','alternate_email','mobile']
+		widgets = {
+		'department' :Select2Widget(attrs={"style":"width:100%"}),
+		'year':Select2Widget(attrs={"style":"width:100%"}),
+		'hostel':Select2Widget(attrs={"style":"width:100%"}),
+		}
