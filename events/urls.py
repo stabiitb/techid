34c085ec130cd459ccd1f23301cd01ac272eb816
prithvirajbilla <prithviraj.m.billa@gmail.com>
2014-01-01 11:44:40 +0530

    :+1: removed wiki

--- events/urls.py
+++ events/urls.py
@@ -40,9 +40,9 @@ urlpatterns = patterns('',
 )+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL,
      document_root=settings.MEDIA_ROOT)
 
-from wiki.urls import get_pattern as get_wiki_pattern
-from django_notify.urls import get_pattern as get_notify_pattern
-urlpatterns += patterns('',
-    (r'^wiki/notify/', get_notify_pattern()),
-    (r'^wiki/', get_wiki_pattern())
-)
+# from wiki.urls import get_pattern as get_wiki_pattern
+# from django_notify.urls import get_pattern as get_notify_pattern
+# urlpatterns += patterns('',
+#     (r'^wiki/notify/', get_notify_pattern()),
+#     (r'^wiki/', get_wiki_pattern())
+# )
