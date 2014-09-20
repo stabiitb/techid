b85fa847e8d20cad29975b52018b10fe153376f9
prateekchandan <prateekchandan5545@gmail.com>
2014-09-20 06:02:21 +0530

    removed tinkerer lab from events :gun:

--- tinkerer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-
-urlpatterns = patterns('',
-	url(r'^$','tinkerer.views.tinkererlogs'),
-	url(r'enter/$','tinkerer.views.timeEnter'),
-	url(r'leave/$','tinkerer.views.timesubmit'),
-	url(r'components/$','tinkerer.views.table_view'),
-	)
